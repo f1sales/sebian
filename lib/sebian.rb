9ea3d6db4fee91d023b068049591372ecccb1a38
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-10-31 10:29:33 -0300

    Git init

---
+++ lib/sebian.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sebian/version"
+
+module Sebian
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/sebian_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Sebian do
+  it "has a version number" do
+    expect(Sebian::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
